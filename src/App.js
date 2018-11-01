c1718d074338f3c8c8e6c87a46b78c75813de22c
thelannisters <ntrung.hcmus@gmail.com>
2018-11-01 19:01:44 +0700

    update login google

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, compose } from 'redux'
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
 import firebase from 'firebase'
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+import Login from './Components/Login'
+const firebaseConfig = {
+  apiKey: "AIzaSyAschOGA0-8sTpxuWfICuVjv64xXYNXxFk",
+    authDomain: "s1mplechat-1480e.firebaseapp.com",
+    databaseURL: "https://s1mplechat-1480e.firebaseio.com",
+    projectId: "s1mplechat-1480e",
+    storageBucket: "s1mplechat-1480e.appspot.com",
+    messagingSenderId: "586571699709"
 }
+
+const rrfConfig = {
+  userProfile: 'users',
+  enableLogging: false, 
+}
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), 
+)(createStore)
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+})
+
+const initialState = {}
+const store = createStoreWithFirebase(rootReducer, initialState)
+
+const App = () => (
+  <Provider store={store}>
+    <Login />
+  </Provider>
+);
+
 
 export default App;
